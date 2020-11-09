ede0d0f4e668eb8c39e9ea6f8586c8591ca604d6
zvanclee <zachvancleef@gmail.com>
2020-11-08 21:43:46 -0500

    6A & 6B seperated

---
+++ HW 6A (last commits before due date)/js/myScripts.js
@@ -0,0 +1,40 @@
+// product details page
+// color customization function
+// -pre: page always loads on 'strawberry' color
+// -post: color of images and text by color field will reflect newly selected color
+function changeColor(newColor) {
+  let newProductImg = document.getElementById("product-img");
+  let colorFieldText = document.getElementById("color-text");
+
+  if(newColor == "strawberry") {
+    newProductImg.src = "images/strawberry-harness.png";
+    colorFieldText.textContent="Strawberry";
+  }
+  else if (newColor == "blackberry") {
+    newProductImg.src = "images/blackberry-harness.png";
+    colorFieldText.textContent="Blackberry";
+  }
+  else if (newColor == "crazyberry") {
+    newProductImg.src = "images/crazyberry-harness.png";
+    colorFieldText.textContent="Crazyberry";
+  }
+  else if (newColor == "fireOrange") {
+    newProductImg.src = "images/fireOrange-harness.png";
+    colorFieldText.textContent="Fire Orange";
+  }
+}
+
+// shows message that item has been added to cart
+function addItemToCartMessage() {
+  alert("Item added to cart!")
+}
+
+function addItemCartCount() {
+  let displayedItemCount = document.getElementById("item-count-in-cart");
+  let currItemCount = displayedItemCount.innerHTML;
+  currItemCount = parseInt(currItemCount, 10);
+  let newItemCount = currItemCount + 1;
+  let strNewItemCount = newItemCount.toString();
+  displayedItemCount.innerHTML = strNewItemCount;
+  document.getElementById("item-count-in-cart").style.display = "inline";
+}
